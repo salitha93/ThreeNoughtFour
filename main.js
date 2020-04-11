fa673e1cefbaf0158ff42ad6fa0bfc59b06acf3c
salitha93 <salitha12@gmail.com>
2020-04-11 07:15:11 +0530

    Modied and run successfully

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 console.log("hello world");
+
+FBInstant.initializeAsync()
+  .then(function()
+  {
+      
+  // Start loading game assets here
+    var progress = 0;
+
+    setInterval(function()
+    {
+        progress+=3;
+        FBInstant.setLoadingProgress(progress);
+    },100);
+
+    console.log("loaded");
+  }
+);
+
+//https://www.facebook.com/embed/instantgames/2856444467764438/player?game_url=https://localhost:8080
